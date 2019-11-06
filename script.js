9335326a33007dcb259402cbfa724eda9299256d
hallothere <51781811+hallothere@users.noreply.github.com>
2019-11-06 12:10:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,6 @@ function createMovie(title, duration, quote) {
     this.logQuote = () => console.log(quote);                             
 };
 
-
 //2.
 // building the new objects
 var starWars = new createMovie('Star Wars', 121, 'If there\'s a bright center to the universe, you\'re on the planet that it\'s farthest from.');
@@ -24,12 +23,8 @@ var forrestGump = new createMovie('Forrest Gump', 142, 'Life is like a box of ch
 var theWizzardOfOz = new createMovie('The Wizard of Oz', 101, 'Lions and tigers and bears, oh my!');
 var cabaret = new createMovie('Cabaret', 124, 'Life is a cabaret, old chum, so come to the cabaret.');
 
-starWars.logQuote();
-console.log(pulpFiction.isLongerThan(starWars));
-
 //building the movies array
 var movies = [starWars, pulpFiction, dirtyDancing, forrestGump, theWizzardOfOz, cabaret];
-
 
 //3.
 function getMovieByTitle(string) {
@@ -40,15 +35,12 @@ function getMovieByTitle(string) {
     }
 };
 
-
 function getAverageDuration() {
     var array = movies.map(x => x.duration);
     const getSum = (total, cur) => total + cur;
     var sum = array.reduce(getSum);
     return sum / movies.length;
 };
-
-
 
 //another option
 
